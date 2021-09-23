17817ca711139a9e45518e25b7b6db718e0f815b
Muhsin Elmas <mussinelmas@hotmail.com>
2021-09-23 16:13:57 +0300

    Add project files.

---
+++ Yaş_Hesaplama/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Yaş_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli designer değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        ///Designer desteği için gerekli metottur; bu metodun
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(39, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(42, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(87, 70);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(42, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(135, 70);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(55, 21);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Gün";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ay";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Yıl";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(20, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 190);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Doğum Gününü Gir";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(64, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "10.07.2015";
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(3, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 60);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Not :Önce Yılı Giriniz";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(74, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(268, 219);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yaş Hesaplama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Yaş_Hesaplama/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yaş_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 31; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+            for (int a = 1; a <= 12; a++)
+            {
+                comboBox2.Items.Add(a);
+            }
+            for (int b = DateTime.Now.Year; b >= 1900; b--)
+            {
+                comboBox3.Items.Add(b);
+            }
+
+            label5.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gün, ay, yıl;
+            gün = DateTime.Now.Day;
+            ay = DateTime.Now.Month;
+            yıl = DateTime.Now.Year;
+            try
+            {
+                if (DateTime.Now.Day < int.Parse(comboBox1.Text) && DateTime.Now.Month > int.Parse(comboBox2.Text))
+                {
+                    ay = ay - 1;
+                    gün = gün + 30;
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                if (DateTime.Now.Day < int.Parse(comboBox1.Text) && DateTime.Now.Month < int.Parse(comboBox2.Text))
+                {
+                    yıl = yıl - 1;
+                    ay = ay + 12;
+                    ay = ay - 1;
+                    gün = gün + 30;
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    gün = gün - int.Parse(comboBox1.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                else if (DateTime.Now.Month < int.Parse(comboBox2.Text))
+                {
+                    yıl = yıl - 1;
+                    ay = ay + 12;
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                else if (DateTime.Now.Day > int.Parse(comboBox1.Text) && DateTime.Now.Month > int.Parse(comboBox2.Text))
+                {
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                else if (DateTime.Now.Month < int.Parse(comboBox2.Text) && DateTime.Now.Day > int.Parse(comboBox1.Text))
+                {
+                    yıl = yıl - 1;
+                    ay = ay + 12;
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                else if (DateTime.Now.Day == int.Parse(comboBox1.Text) && DateTime.Now.Month == int.Parse(comboBox2.Text))
+                {
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = "Tam " + yıl.ToString() + " Yaşındasınız";
+                }
+                else if (DateTime.Now.Day == int.Parse(comboBox1.Text) && DateTime.Now.Month < int.Parse(comboBox2.Text))
+                {
+                    yıl = yıl - 1;
+                    ay = ay + 12;
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                else if(DateTime.Now.Day == int.Parse(comboBox1.Text) && DateTime.Now.Month > int.Parse(comboBox2.Text))
+                {
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                else if (DateTime.Now.Month == int.Parse(comboBox2.Text) && DateTime.Now.Day < int.Parse(comboBox1.Text))
+                {
+                    yıl = yıl - 1;
+                    ay = ay + 12;
+                    ay = ay - 1;
+                    gün = gün + 30;
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+                else if (DateTime.Now.Month == int.Parse(comboBox2.Text) && DateTime.Now.Day > int.Parse(comboBox1.Text))
+                {
+                    gün = gün - int.Parse(comboBox1.Text);
+                    ay = ay - int.Parse(comboBox2.Text);
+                    yıl = yıl - int.Parse(comboBox3.Text);
+                    label4.Text = yıl.ToString() + " Yaşında " + ay.ToString() + " Aylık " + gün.ToString() + " Günlüksünüz";
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen Doğum Tarihinizi Düzgün Giriniz!!!", "Uyarı");
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.Text != "2015")
+            {
+                comboBox1.Items.Clear();
+                comboBox2.Items.Clear();
+                for (int i = 1; i <= 31; i++)
+                {
+                    comboBox1.Items.Add(i);
+                }
+                for (int a = 1; a <= 12; a++)
+                {
+                    comboBox2.Items.Add(a);
+                }
+            }
+            else
+            {
+                comboBox1.Items.Clear();
+                comboBox2.Items.Clear();
+                for (int i = 1; i < DateTime.Now.Day; i++)
+                {
+                    comboBox1.Items.Add(i);
+                }
+                for (int a = 1; a <= DateTime.Now.Month; a++)
+                {
+                    comboBox2.Items.Add(a);
+                }
+            }
+
+        }
+    }
+}
